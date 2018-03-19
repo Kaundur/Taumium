186987cd07efc09c4bdaf3ecef1b6eefb776ac9c
Kaundur <hello@kaundur.com>
2018-03-19 20:44:07 +0000

    Added simple block creation

--- block.py
+++ block.py
@@ -1,10 +1,31 @@
+import time
+import json
+import hashlib
+
 
 class Block:
-    def __init__(self):
-        pass
+    def __init__(self, transactions, block_index, previous_hash):
+        self.block_index = block_index
+        self.timestamp = time.time()
+        self.transactions = transactions
+        self.proof = None
+        self.previous_hash = previous_hash
+        self._hash = self.hash_block()
+
+    def block_to_json(self):
+        hashable_dict = {
+            'block_index': self.block_index,
+            'timestamp': self.timestamp,
+            'transactions': self.transactions,
+            'proof': self.proof,
+            'previous_hash': self.previous_hash
+        }
+        block_json = json.dumps(hashable_dict, sort_keys=True).encode()
+        return block_json
 
     def hash_block(self):
-        pass
+        block_json = self.block_to_json()
+        return hashlib.sha256(block_json).hexdigest()
 
     def __repr__(self):
-        return '<Block>'
+        return '<Block %s> : %s' % (self.block_index, self._hash)
--- blockChain.py
+++ blockChain.py
@@ -5,3 +5,23 @@ class BlockChain:
     def __init__(self):
         self.chain = []
         self.transactions = []
+
+        # Create genesis block
+        self.new_block(0, '')
+
+    def last_block(self):
+        return self.chain[-1]
+
+    def new_block(self, block_index, previous_hash):
+        self.chain.append(block.Block(self.transactions, block_index, previous_hash))
+        self.transactions = []
+
+        print('New block created')
+        print(self.last_block())
+
+    def add_transaction(self, sender, recipient, amount):
+        self.transactions.append({
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount
+        })
